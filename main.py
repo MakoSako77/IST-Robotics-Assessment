05f48bbbc5aa2d22bc5aa3284819c82f8c6db0fd
Xavier Rowley <xavierrowley@protonmail.com>
2022-09-23 11:37:47 +1000

    This might work now

--- main.py
+++ main.py
@@ -1,63 +1,37 @@
-  #!/usr/bin/env pybricks-micropython
-  from pybricks.hubs import EV3Brick
-  from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                  InfraredSensor, UltrasonicSensor, GyroSensor)
-  from pybricks.parameters import Port, Stop, Direction, Button, Color
-  from pybricks.tools import wait, StopWatch, DataLog
-  from pybricks.robotics import DriveBase
-  from pybricks.media.ev3dev import SoundFile, ImageFile
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor,InfraredSensor, UltrasonicSensor, GyroSensor
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
-  # This program requires LEGO EV3 MicroPython v2.0 or higher.
-  # Click "Open user guide" on the EV3 extension tab for more information.
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+# Create your objects here.
+ev3 = EV3Brick()
+LMotor = Motor(Port.B)
+RMotor = Motor(Port.C)
+US = UltrasonicSensor(Port.S4)
+CS = ColorSensor(Port.S3)
+ARM = Motor(Port.D)
+robot = DriveBase(LMotor, RMotor, wheel_diameter=50, axle_track=126)
+#Calibrate this :)  no share thx comsdfadfladhlasjdlsajdklsajd 
+# Calculate the light threshold. Choose values based on your measurements.
+black = 10
+white = 85
+threshold = (black+white)/2
 
 
-  # Create your objects here.
-  ev3 = EV3Brick()
+# Start following the line endlessly.
+while CS.color != COLOR.RED: #
+    ARM.run(360)
+    if CS.reflection() < threshold:
+         robot.drive(40,50)
+    else:
+         robot.drive(40,-50)
 
 
-  # Write your program here.
-  left_motor = Motor(Port.B)
-  right_motor = Motor(Port.C)
-  line_sensor = ColorSensor(Port.S3)
-  spin_motor = Motor(Port.D)
-
-  #Begin Drivebase
-  robot = DriveBase(right_motor, left_motor, wheel_diameter=55.5, axle_track=104)
-
-
-  #Main
-
-  #Start Motor
-while True:
-    spin_motor.run_target(1000, 36000)
-
-#Calcualte Light Threshold     
-BLACK = 9
-WHITE = 85
-threshold = (BLACK + WHITE) / 2
-
-#Set the Drive Speed
-DRIVE_SPEED = 100
-
-#Proportional Gain for Line Control
-PROPORTIONAL_GAIN = 1.2
-
-
-    #Line Sensor
-while True:
-
-  #Calculate Deviation from Line
-   deviation = line_sensor.reflection() - threshold
-
-   #Calculate Turn Rate
-    turn_rate = PROPORTIONAL_GAIN * deviation
-
-    #Set the Drive base speed and turn rate
-    robot.drive(DRIVE_SPEED, turn_rate)
-
-    wait(10)
-
-        
-  while True:
-    spin_motor.run_target(1000, 36000)
+wait(10)
